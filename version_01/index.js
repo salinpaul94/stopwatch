90a73c056bfd3de1a9b128596dce2981564b1b67
salinpaul valooran <salinpv@hotmail.com>
2023-10-30 23:21:27 +0530

    using variables h, m , s to display time in 00:00:00 format

--- version_01/index.js
+++ version_01/index.js
@@ -12,7 +12,10 @@ function stopwatch() {
     minutes = 0;
     hours++;
   }
-  displayTime.innerText = `${hours}:${minutes}:${seconds}`
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  displayTime.innerHTML = h + ":" + m + ":" + s;
 }
 
 function watchStart() {
